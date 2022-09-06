378b991700bd1599c2053fd4f5df7e62c07fafc2
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 15:24:35 +0200

    remove edit

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ window.onload = (event) => {
     var l = doc.documentElement.innerHTML;
     l.replace('href="/wiki','href="http://wiki.hisgis.nl/wiki');
     l.replace('src="/images','src="http://wiki.hisgis.nl/wiki/images')
-    console.log(l);
+    l.replace('src="<span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/index.php?title=Basis&amp;veaction=edit&amp;section=1" class="mw-editsection-visualeditor" title="Deelpagina bewerken: Basis">bewerken</a><span class="mw-editsection-divider"> | </span><a href="/index.php?title=Basis&amp;action=edit&amp;section=1" title="Deelpagina bewerken: Basis">brontekst bewerken</a><span class="mw-editsection-bracket">]</span></span>','');
     document.body.innerHTML = document.body.innerHTML + l;
   };
