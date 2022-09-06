2180d2571e4613c0bdcc160842dd301fce9698a0
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 15:26:33 +0200

    persistent

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ window.onload = (event) => {
     var t = GetWiki("Basis");
     var doc = new DOMParser().parseFromString(t, "text/html");
     var l = doc.documentElement.innerHTML;
-    l.replace('href="/wiki','href="http://wiki.hisgis.nl/wiki');
-    l.replace('src="/images','src="http://wiki.hisgis.nl/wiki/images')
-    l.replace('src="<span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/index.php?title=Basis&amp;veaction=edit&amp;section=1" class="mw-editsection-visualeditor" title="Deelpagina bewerken: Basis">bewerken</a><span class="mw-editsection-divider"> | </span><a href="/index.php?title=Basis&amp;action=edit&amp;section=1" title="Deelpagina bewerken: Basis">brontekst bewerken</a><span class="mw-editsection-bracket">]</span></span>','');
+    l = l.replace('href="/wiki','href="http://wiki.hisgis.nl/wiki');
+    l = l.replace('src="/images','src="http://wiki.hisgis.nl/wiki/images')
+    l = l.replace('src="<span class="mw-editsection"><span class="mw-editsection-bracket">[</span><a href="/index.php?title=Basis&amp;veaction=edit&amp;section=1" class="mw-editsection-visualeditor" title="Deelpagina bewerken: Basis">bewerken</a><span class="mw-editsection-divider"> | </span><a href="/index.php?title=Basis&amp;action=edit&amp;section=1" title="Deelpagina bewerken: Basis">brontekst bewerken</a><span class="mw-editsection-bracket">]</span></span>','');
     document.body.innerHTML = document.body.innerHTML + l;
   };
