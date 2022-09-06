ab96fb3da8afe29d2415b382e29affc6a9edff0b
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 16:01:40 +0200

    stijl en layout

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function GetWiki(pagina){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET","https://wiki.hisgis.nl/api.php?action=parse&page="+pagina+"&prop=text&format=json",false);
     Httpreq.send(null);
-    return JSON.parse(Httpreq.responseText)['parse']['text']['*'];
+    var r = JSON.parse(Httpreq.responseText)['parse']['text']['*'];
+    var doc = new DOMParser().parseFromString(r, "text/html");
+    var l = doc.documentElement.innerHTML;
+    const re_edit = new RegExp(/<span class="mw-editsection">.*?\]<\/span><\/span>/, 'g');
+    l = l.replaceAll(re_edit,'');
+    l = l.replaceAll('href="/wiki','href="https://wiki.hisgis.nl/wiki');
+    l = l.replaceAll('src="/images','src="https://wiki.hisgis.nl/images')
+    return l
 }
 
 window.onload = (event) => {
-    var t = GetWiki("Basis");
-    var doc = new DOMParser().parseFromString(t, "text/html");
-    var l = doc.documentElement.innerHTML;
-    const re = new RegExp(/<span class="mw-editsection">.*?\]<\/span><\/span>/, 'g');
-    l = l.replaceAll(re,'');
-    l = l.replaceAll('href="/wiki','href="https://wiki.hisgis.nl/wiki');
-    l = l.replaceAll('src="/images','src="https://wiki.hisgis.nl/images')
-    document.body.innerHTML = document.body.innerHTML + l;
+    var inhoud = document.body.getElementById("inhoud").innerHTML
+    var basis = GetWiki("Basis");
+    inhoud += basis;
   };
