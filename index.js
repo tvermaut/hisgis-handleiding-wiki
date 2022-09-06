e32288f2ab963115390fac0c0b6f02dbdc7eb6e0
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 14:09:26 +0200

    JSON-request

---
+++ index.js
@@ -0,0 +1,11 @@
+function GetWiki(pagina){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET","https://wiki.hisgis.nl/api.php?action=parse&page="+pagina+"&prop=text&format=json",false);
+    Httpreq.send(null);
+    return JSON.parse(Httpreq.responseText);
+}
+
+window.onload = (event) => {
+    var t = GetWiki("Basis").text;
+    document.body.innerHTML = document.body.innerHTML + t;
+  };
