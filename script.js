f90a3dac45fa996c7cac8fff2a025e17e5942a67
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 15:12:03 +0200

    parse

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function GetWiki(pagina){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET","https://wiki.hisgis.nl/api.php?action=parse&page="+pagina+"&prop=text&format=json",false);
     Httpreq.send(null);
-    return JSON.parse(Httpreq.responseText);
+    return JSON.parse(Httpreq.responseText)['parse']['text']['*'];
 }
 
 window.onload = (event) => {
     var t = GetWiki("Basis");
-    console.log(t);
-    document.body.innerHTML = document.body.innerHTML + t;
+    var doc = new DOMParser().parseFromString(t, "text/html");
+    var l = doc.documentElement.textContent;
+    console.log(l);
+    document.body.innerHTML = document.body.innerHTML + l;
   };
