565ccaead45051c0cd359b6b07b9c774a8a6f1a9
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-06 17:40:42 +0200

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function GetWiki(pagina){
     const re_edit = new RegExp(/<span class="mw-editsection">.*?\]<\/span><\/span>/, 'g');
     l = l.replaceAll(re_edit,'');
     l = l.replaceAll('href="/wiki','href="https://wiki.hisgis.nl/wiki');
-    l = l.replaceAll('src="/images','src="https://wiki.hisgis.nl/images')
+    l = l.replaceAll('src="/images','src="https://wiki.hisgis.nl/images');
     return l
 }
 
@@ -16,8 +16,8 @@ window.onload = (event) => {
     var basis = GetWiki("Basis");
     document.getElementById("inhoud").innerHTML = document.getElementById("inhoud").innerHTML + basis;
 
-    var miid = GetWiki("Multipolygonen_in_iD");
-    document.getElementById("inhoud").innerHTML = document.getElementById("inhoud").innerHTML + miid;
+    //var miid = GetWiki("Multipolygonen_in_iD");
+    //document.getElementById("inhoud").innerHTML = document.getElementById("inhoud").innerHTML + miid;
 
     Toc.init({$nav: $("#toc"), $scope: $("h1, h2, h3, h4, h5")});
     $("body").scrollspy({target: "#toc"});
