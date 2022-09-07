7b6b9a1b2a37533d42e8ca6e73ff25f292a3df1e
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-07 16:09:52 +0200

    toc

--- script.js
+++ script.js
@@ -22,20 +22,10 @@ window.onload = (event) => {
     var miid = GetWiki("Multipolygonen_in_iD");
     i.innerHTML += miid;
 
-    Toc.init({$nav: $("#toc"), $scope: $("h1, h2, h3, h4, h5")});
+    Toc.init({$nav: $("#toc"), $scope: $(document.body)});//$scope: $("h1, h2, h3, h4, h5")
     //$("body").scrollspy({target: "#toc"});
     
     $('[data-toggle="tooltip"]').tooltip()
     //$('[data-spy="scroll"]').each(function () {var $spy = $(this).scrollspy('refresh')})
-
-    Toc.init({
-
-        // The element that the navigation will be created in
-        $nav: $('#toc'),
-      
-        // The element where the search for headings will be limited to
-        $scope: $(document.body)
-        
-      });
 
   };
