9342d66fdc72f6cd6b103cfcd9cbb3fa7c17c54a
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-17 21:47:24 +0200

    haakjes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function GetWiki(pagina){
     var Httpreq = new XMLHttpRequest();
     Httpreq.open("GET","https://wiki.hisgis.nl/api.php?action=parse&page="+pagina+"&prop=text&format=json",false);
     Httpreq.send(null);
-    var r = (JSON.parse(Httpreq.responseText))['parse']['text']['*'];
+    var r = JSON.parse(Httpreq.responseText)['parse']['text']['*'];
     var doc = new DOMParser().parseFromString(r, "text/html");
     var l = doc.documentElement.innerHTML;
     const re_edit = new RegExp(/<span class="mw-editsection">.*?\]<\/span><\/span>/, 'g');
