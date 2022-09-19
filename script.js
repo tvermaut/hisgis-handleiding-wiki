c8971e57fece7b827d8ab413872712de69319b60
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-19 11:29:48 +0200

    toc

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ window.onload = (event) => {
         i.innerHTML += GetWiki(p);
         }
         
+    $("#inhoudsopgave").toc({content: "inhoud", headings: "h1,h2,h3,h4,h5,h6"});
     //Toc.init({$nav: $("#toc"), $scope: $("h2, h3, h4, h5")});//$scope: $("h1, h2, h3, h4, h5")//document.body
     //$("body").scrollspy({target: "#toc"});
     
